d5959d612350a395477ffc5e6262df804234e7e4
Steven Lewis <slewi1@monsanto.com>
2018-12-13 20:05:32 -0700

    Day 1

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "advent-2018"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/day1.scala
@@ -0,0 +1,18 @@
+import scala.io.Source
+
+object day1 extends App {
+
+  val input = Source.fromResource("day1.txt").getLines()
+  val frequencyChanges = input.map(_.toInt).toVector
+
+  val part1 = frequencyChanges.sum
+  println(s"PART 1: $part1")
+
+  def findFirstDuplicate(changes: Vector[Int], currentFreq: Int, position: Int = 1, seen: Set[Int] = Set()): Int = {
+    if (seen.contains(currentFreq)) currentFreq
+    else findFirstDuplicate(changes, currentFreq + changes(position % changes.size), position + 1, seen + currentFreq)
+  }
+
+  val part2 = findFirstDuplicate(frequencyChanges, frequencyChanges.head)
+  println(s"PART 2: $part2")
+}

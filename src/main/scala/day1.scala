6d7dbe4c8fd6d1b31b03a644177f2a322722135e
Steven Lewis <slewi1@monsanto.com>
2018-12-13 20:13:03 -0700

    Specify tail recursion

--- src/main/scala/day1.scala
+++ src/main/scala/day1.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import scala.io.Source
 
 object day1 extends App {
@@ -8,6 +9,7 @@ object day1 extends App {
   val part1 = frequencyChanges.sum
   println(s"PART 1: $part1")
 
+  @tailrec
   def findFirstDuplicate(changes: Vector[Int], currentFreq: Int, position: Int = 1, seen: Set[Int] = Set()): Int = {
     if (seen.contains(currentFreq)) currentFreq
     else findFirstDuplicate(changes, currentFreq + changes(position % changes.size), position + 1, seen + currentFreq)

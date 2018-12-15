80f3298d93b4af17949d64d83314aa5d9067df59
Steven Lewis <slewi1@monsanto.com>
2018-12-14 17:45:47 -0700

    Refactor to use map

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -5,7 +5,7 @@ object day3 extends App {
   val input = Source.fromResource("day3.txt").getLines()
 
   case class Claim(id: Int, x: Int, y: Int, width: Int, height: Int) {
-    val allCoordinates: Set[(Int, Int)] = (x until (x + width)).flatMap { x => (y until (y + height)).map { y => x -> y }}.toSet
+    val allCoordinates: Set[(Int, Int)] = (x until x + width).flatMap(x => (y until y + height).map(x -> _)).toSet
   }
 
   val claimRegex = """#(\d+) @ (\d+),(\d+): (\d+)x(\d+)""".r
@@ -14,11 +14,11 @@ object day3 extends App {
     case claimRegex(id, x, y, width, height) => Claim(id.toInt, x.toInt, y.toInt, width.toInt, height.toInt)
   }
 
-  val part1 = claims.flatMap(_.allCoordinates).groupBy(identity).count(_._2.length > 1)
+  val pointCountMap = claims.flatMap(_.allCoordinates).groupBy(identity).mapValues(_.size)
+  val part1 = pointCountMap.count(_._2 > 1)
   println(s"PART 1: $part1")
 
-  val noSharedCoordinates = (claim1: Claim, claim2: Claim) => claim1.allCoordinates.intersect(claim2.allCoordinates).isEmpty
-  val isAlone = (claim: Claim) => claims.filter(_ != claim).forall(noSharedCoordinates(_, claim))
+  val isAlone = (claim: Claim) => claim.allCoordinates.forall(pointCountMap(_) == 1)
   val part2 = claims.find(isAlone).get.id
 
   println(s"PART 2: $part2")

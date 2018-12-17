13d453d2e13cc3f36140e18d0e73a8c1218f9830
Steven Lewis <slewi1@monsanto.com>
2018-12-16 17:05:42 -0700

    Cleanup day 6

--- src/main/scala/day6.scala
+++ src/main/scala/day6.scala
@@ -15,43 +15,36 @@ object day6 extends App {
   val rightMost = mappedCoordinates.map(_.x).max + 1
   val bottomMost = mappedCoordinates.map(_.y).max + 1
 
+  //Form a square that surrounds all mapped coordinates, the graph is a list of all those points
   val graph = (leftMost to rightMost).flatMap(x => (topMost to bottomMost).map(y => Coordinate(x, y)))
 
   def manhattanDistance(a: Coordinate, b: Coordinate): Int = Math.abs(a.x - b.x) + Math.abs(a.y - b.y)
 
   def closestCoordinate(point: Coordinate): Option[Coordinate] = {
-    val mappedCoordinateDistance = mappedCoordinates.map { coordinate =>
-      coordinate -> manhattanDistance(point, coordinate)
-    }
-    mappedCoordinateDistance.sortBy(_._2) match {
+    mappedCoordinates
+      .map(coordinate => coordinate -> manhattanDistance(point, coordinate))
+      .sortBy(_._2) match {
       case a :: b :: _ if a._2 != b._2 => Some(a._1)
       case _ => None
     }
   }
 
-  val closestMappedCoordinates = graph.map { point =>
-    point -> closestCoordinate(point)
-  }
+  val closestMappedCoordinates = graph.map(point => point -> closestCoordinate(point))
 
+  //Any mapped coordinate which is the closest point on the edge of the graph is assumed infinite area
   val infiniteMappedCoordinates = closestMappedCoordinates.filter { case (point, _) =>
     point.x == leftMost || point.x == rightMost || point.y == topMost || point.y == bottomMost
   }.flatMap(_._2).toSet
 
   val finiteMappedCoordinates = mappedCoordinates.filterNot(infiniteMappedCoordinates.contains)
 
-  val areaOfFiniteMappedCoordinates = finiteMappedCoordinates.map { coordinate =>
-    coordinate -> closestMappedCoordinates.count(_._2.contains(coordinate))
-  }
+  def area(coordinate: Coordinate): Int = closestMappedCoordinates.count(_._2.contains(coordinate))
 
-  val part1 = areaOfFiniteMappedCoordinates.map(_._2).max
+  val part1 = finiteMappedCoordinates.map(area).max
   println(s"PART 1: $part1")
 
   def totalDistance(point: Coordinate): Int = mappedCoordinates.map(manhattanDistance(_, point)).sum
 
-  val totalDistances = graph.map { point =>
-    point -> totalDistance(point)
-  }
-
-  val part2 = totalDistances.count(_._2 < 10000)
+  val part2 = graph.map(totalDistance).count(_ < 10000)
   println(s"PART 2: $part2")
 }
